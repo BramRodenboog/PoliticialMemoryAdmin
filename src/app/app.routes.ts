721fae8a691428530716cac3d51e411c42194975
Bram Rodenboog <BramRodenboog@users.noreply.github.com>
2026-06-15 11:27:30 +0200

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { AdminPage } from './pages/admin-page/admin-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomePage,
+    },
+    {
+        path: 'admin',
+        component: AdminPage,
+    }
+];
